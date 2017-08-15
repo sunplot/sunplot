0f97fd04e6a8579f9036e2b9ad793f516ba173ac
Michael Suzuki <michael.suzuki@alfresco.com>
2017-08-15 13:32:56 +0100

    Adding es6 transpile to test specs

---
+++ test/components/dialogs/SettingDialog.spec.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { mount, shallow } from 'enzyme';
+// import {expect} from 'chai';
+// import SettingDialog from '../../../src/components/dialogs/SettingDialog';
+//
+// describe('a passing test', () => {
+//   it('should pass', () => {
+//     expect(true).to.be.true;
+//   });
+// });
---
+++ test/test.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import {expect} from 'chai';
+
+describe('a passing test', () => {
+  it('should pass', () => {
+    expect(true).to.be.true;
+  });
+});
